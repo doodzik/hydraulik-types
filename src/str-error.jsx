ff0d26b03dd666afdf1231c525b683f617801240
doodzik <frederik.dudzik@gmail.com>
2015-05-28 13:40:44 +0200

    mv types from hydraulik types

---
+++ src/str-error.jsx
@@ -0,0 +1,13 @@
+import Type from './type'
+
+export default class StrError extends Type {
+  constructor(value, options = {}){
+    super(value, options)
+    this.length = value.length
+  }
+
+  validation(){
+    if (this.length != 0)
+      return 'StrError: value should be of length 0'
+  }
+}
---
+++ src/types.jsx
@@ -0,0 +1,3 @@
+export var Str       = require('./str')
+export var StrError  = require('./str-error')
+export var Type      = require('./type')
