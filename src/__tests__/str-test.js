322ed81cb6d560e43bfc967a0638f52879df871d
doodzik <frederik.dudzik@gmail.com>
2015-06-13 14:06:36 +0200

    add name of Type to Str error msg & let Str accept all str lengths

--- src/__tests__/str-test.js
+++ src/__tests__/str-test.js
@@ -11,11 +11,15 @@ describe('string', function() {
 
   it('#validate returns string string too long', function() {
     var str = new Array(130).join('a')
-    expect(new Str(str).validate()).toEqual('Str is too long')
+    var strInstance = new Str(str)
+    strInstance.max = 120
+    expect(strInstance.validate()).toEqual('Str is too long')
   })
 
   it('#validate returns string is too short', function() {
     var str = ''
-    expect(new Str(str).validate()).toEqual('Str is too short')
+    var strInstance = new Str(str)
+    strInstance.min = 5 
+    expect(strInstance.validate()).toEqual('Str is too short')
   })
 })
--- src/str.jsx
+++ src/str.jsx
@@ -3,15 +3,16 @@ import Type from './type'
 export default class Str extends Type {
   constructor(...args){
     super(...args)
+    this.value  = this.value.toString()
     this.length = this.value.length
-    this.min = 1
-    this.max = 128
+    this.min    = -1
+    this.max    = -1
   }
 
   validation(){
-    if (this.length < this.min)
-      return 'Str is too short'
-    if (this.length > this.max)
-      return 'Str is too long'
+    if (this.min > -1 && this.length < this.min)
+        return `${this.name} is too short`
+    if (this.max > -1 && this.length > this.max)
+        return `${this.name} is too long`
   }
 }
